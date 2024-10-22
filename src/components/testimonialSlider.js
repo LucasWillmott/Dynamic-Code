ee6c4f20b7eb1f316b1ed0d1f8aeef02a25106b2
Lucas <lucasxboxlive@hotmail.co.uk>
2024-10-22 10:30:22 +0100

    data-set test

--- src/components/testimonialSlider.js
+++ src/components/testimonialSlider.js
@@ -1,3 +1,5 @@
 export default function slider () {
+  const slider = document.querySelector('[data-testimonialSlider]')
   console.log('testimonialSlider')
+  console.log(slider.dataset.testimonialSlider)
 }
