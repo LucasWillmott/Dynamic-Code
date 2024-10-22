7c2845353f6d5ddb05f695ed1d604e0c53425b1d
Lucas <lucasxboxlive@hotmail.co.uk>
2024-10-22 10:03:38 +0100

    init

---
+++ src/components/accordions.js
@@ -0,0 +1,4 @@
+export default function accordions () {
+    console.log('accordions')
+  }
+  
--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-import Home from './pages/home'
-import testSection from './sections/testSection'
 // import { initializeGsap } from './utils/initialize-gsap'
 // const { gsap, Flip, CustomEase } = initializeGsap()
 
-// Pages
-const home = document.querySelector('body').classList.contains('body--home')
-if (home) {
-  Home()
+// main.js
+document.fonts.ready.then(() => {
+  checkAndLoadComponents()
+})
+
+// Array of component attribute names
+const componentNames = ['accordions', 'slider']
+
+// Function to check for elements and dynamically import corresponding modules
+function checkAndLoadComponents () {
+  componentNames.forEach(async (name) => {
+    const selector = `[${name}]` // Create selector based on the attribute name
+    if (document.querySelector(selector)) {
+      const module = await import(`./components/${name}.js`) // Dynamically load the module
+      if (typeof module.default === 'function') {
+        module.default() // Run the default exported function
+      }
+    }
+  })
 }
-
-// Sections
-testSection()
-
-// Global Code
-console.log('global')
